8d56946e88892c9912e9328712f18f34b17568f2
Aldrea Rabelo <aldrea.mno@gmail.com>
2020-08-05 17:18:58 -0300

    Adicionando testes na aplicação

--- cypress/integration/testing_cypress.spec.js
+++ cypress/integration/testing_cypress.spec.js
@@ -1,7 +1,35 @@
 describe("Nosso hello ao cypress!", () => {
     //beforeEach(() => cy.visit ("https://example.cypress.io"));
     it("Navegando pelo site de teste do cypress", () =>{
-        cy.visit ("https://example.cypress.io");
+        cy.visit ("https://example.cypress.io")
         //cy.get("css=.search-global-typeahead__input").type("cypress");
-    });
-});
+    })
+
+    it ("Teste após inspeção via navegação do aplicativo do cypress", () =>{
+        cy.get('.home-list > :nth-child(2) > :nth-child(1)')
+    })
+
+    it("Encontrando algo de conteúdo na página", () => {
+        cy.contains('type')
+    })
+    
+    it('Clique no link "type"', () => {
+        //cy.visit('https://example.cypress.io')
+    
+        cy.contains('type').click()
+      })
+    
+    it('Trabalhando com ações com get, types e asserts', () => {
+        cy.visit('https://example.cypress.io')
+    
+        cy.contains('type').click()
+    
+        // Should be on a new URL which includes '/commands/actions'
+        cy.url().should('include', '/commands/actions')
+    
+        // Get an input, type into it and verify that the value has been updated
+        cy.get('.action-email')
+          .type('fake@email.com')
+          .should('have.value', 'fake@email.com')
+      })
+})
